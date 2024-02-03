f54c3c77001b43539e7954e5056c8d8a952a2ac8
BrandonHernandez2708 <brandon.her27@gmail.com>
2024-02-02 19:12:16 -0600

    pag 64

---
+++ km a millas.py
@@ -0,0 +1,4 @@
+km=input("ingrese la cantidad de kilometro que quiere convertir a millas: ")
+kilometros=int(km)
+millas=kilometros/ 0.6214
+print("Los kilometros equivalen en millas a ",round(millas,2))
